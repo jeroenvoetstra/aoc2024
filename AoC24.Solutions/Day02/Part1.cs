75c3deb0365ecacff66c54c60b99e3288e5622c5
Jeroen Voetstra <jeroen.voetstra@tauw.com>
2024-12-02 19:53:28 +0100

    Day 2

---
+++ AoC24.Solutions/Day02/Part1.cs
@@ -0,0 +1,25 @@
+﻿namespace AoC24.Solutions.Day02;
+
+public class Part1 : IAoCSolution
+{
+    static readonly string FilePath = $@"{Environment.GetEnvironmentVariable("AOC_HOME")}\Input\0201.txt";
+
+    public long GetResult()
+    {
+        var data = File.ReadAllLines(FilePath)
+            .Select((line) => line.Split(' ').Select((item) => Convert.ToInt32(item)).ToArray())
+            .ToArray();
+
+        var safeReports = data.Where((report) =>
+        {
+            var levelDiff = new int[report.Length - 1];
+            for (var index = 0; index < report.Length - 1; index++)
+            {
+                levelDiff[index] = report[index + 1] - report[index];
+            }
+            return (levelDiff.All((item) => item < 0) || levelDiff.All((item) => item > 0)) && levelDiff.All((item) => Math.Abs(item) >= 1 && Math.Abs(item) <= 3);
+        }).Count();
+
+        return safeReports;
+    }
+}
---
+++ AoC24.Solutions/Day02/Part2.cs
@@ -0,0 +1,48 @@
+﻿namespace AoC24.Solutions.Day02;
+
+public class Part2 : IAoCSolution
+{
+    private static readonly string FilePath = $@"{Environment.GetEnvironmentVariable("AOC_HOME")}\Input\0201.txt";
+
+    private const int MinIncrement = 1;
+    private const int MaxIncrement = 3;
+
+    public long GetResult()
+    {
+        var data = File.ReadAllLines(FilePath)
+            .Select((line) => line.Split(' ').Select((item) => Convert.ToInt32(item)).ToArray())
+            .ToArray();
+
+        var safeReports = data.Where((report) =>
+        {
+            var result = IsAcceptableEquidistance(report, MinIncrement, MaxIncrement);
+
+            // Brute force approach, it took too long to fix properly
+            if (!result)
+            {
+                for (var index = 0; index < report.Length; index++)
+                {
+                    result = IsAcceptableEquidistance(report.Where((item, i) => i != index).ToArray(), MinIncrement, MaxIncrement);
+                    if (result)
+                        break;
+                }
+            }
+
+            return result;
+
+        }).Count();
+
+        return safeReports;
+    }
+
+    private static bool IsAcceptableEquidistance(int[] source, int minDistanceInclusive, int maxDistanceInclusive)
+    {
+        var levelDiff = new int[source.Length - 1];
+        for (var index = 0; index < source.Length - 1; index++)
+        {
+            levelDiff[index] = source[index + 1] - source[index];
+        }
+
+        return (levelDiff.All((item) => item < 0) || levelDiff.All((item) => item > 0)) && levelDiff.All((item) => Math.Abs(item) >= minDistanceInclusive && Math.Abs(item) <= maxDistanceInclusive);
+    }
+}
